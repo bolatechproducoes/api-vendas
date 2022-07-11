7aa4b8713e60965c7dfa79df94324748b84248c9
Gustavo <bolatechproducoes@gmail.com>
2022-07-11 17:38:31 -0300

    Middleware de tratamento de erros

---
+++ src/shared/errors/AppError.ts
@@ -0,0 +1,12 @@
+//Classe que trata os erros (Trabalha junto com middleware para fazer o try catch)
+class AppError {
+  public readonly message: string;
+  public readonly statusCode: number;
+
+  constructor(message: string, statusCode = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+export default AppError;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,14 +1,34 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+//Habilita as rotas definidas em src/shared/http/routes/index.ts
 app.use(routes);
 
+// Middleware de tratamento de erro
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
+
+//Habilita o express na porta 3333
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
   console.log('Server started on port 3333! http://localhost:3333/');
