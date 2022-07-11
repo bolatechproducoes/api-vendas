2fb89c7c6e0b0b6942d07bd866566572a8a9a632
Gustavo <bolatechproducoes@gmail.com>
2022-07-11 16:48:58 -0300

    Config Express - Cors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,15 @@
-console.log('Hello Dev!');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server started on port 3333! http://localhost:3333/');
+});
