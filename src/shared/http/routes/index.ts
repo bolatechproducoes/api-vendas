f08a1cc4b7f177d932a314ba78cae83c5aff4848
Gustavo <bolatechproducoes@gmail.com>
2022-07-14 15:39:46 -0300

    productsRoutes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import productsRoutes from '@modules/products/routes/products.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello Dev' });
-});
+routes.use('/products', productsRoutes);
 
 export default routes;
