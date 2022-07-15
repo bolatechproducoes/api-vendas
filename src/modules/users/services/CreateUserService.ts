f052a9f816781f26f65637ff7cbee788a8443ba4
Gustavo <bolatechproducoes@gmail.com>
2022-07-15 15:21:46 -0300

    passwordHash com bcryptjs

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -18,10 +19,13 @@ class CreateUserService {
       throw new AppError('Email address already used.');
     }
 
+    // Criptografa a senha
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
