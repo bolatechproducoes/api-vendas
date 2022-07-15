44d7b071946770c5518bdcf0355bd496af62e47f
Gustavo <bolatechproducoes@gmail.com>
2022-07-15 14:55:12 -0300

    users.routes.ts

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import productsRoutes from '@modules/products/routes/products.routes';
+import usersRoutes from '@modules/users/routes/users.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRoutes);
+routes.use('/users', usersRoutes);
 
 export default routes;
