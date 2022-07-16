be1460e05b17dbac1cd580be6989b4eeaed14af5
Gustavo <bolatechproducoes@gmail.com>
2022-07-16 11:20:05 -0300

    sessions.routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import productsRoutes from '@modules/products/routes/products.routes';
 import usersRoutes from '@modules/users/routes/users.routes';
+import sessionsRouter from '@modules/users/routes/sessions.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRoutes);
 routes.use('/users', usersRoutes);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
