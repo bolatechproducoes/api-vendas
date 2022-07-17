c2b5f2fa6d366c68c388d6aa036b189ef67f2245
Gustavo <bolatechproducoes@gmail.com>
2022-07-17 16:29:08 -0300

    password.routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import productsRoutes from '@modules/products/routes/products.routes';
 import usersRoutes from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
+import passwordRouter from '@modules/users/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRoutes);
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
