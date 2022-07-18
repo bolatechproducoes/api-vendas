386c9514d63c5c9ac328810799e32c52fb720550
Gustavo <bolatechproducoes@gmail.com>
2022-07-18 11:30:57 -0300

    profile.routes.ts

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import productsRoutes from '@modules/products/routes/products.routes';
 import usersRoutes from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
+import profileRouter from '@modules/users/routes/profile.routes';
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use('/products', productsRoutes);
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
