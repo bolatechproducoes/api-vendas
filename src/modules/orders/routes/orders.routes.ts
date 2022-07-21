c782c87b98af0b564d18b379636f6f90081b5e35
Gustavo <bolatechproducoes@gmail.com>
2022-07-21 15:52:03 -0300

    orders.routes

---
+++ src/modules/orders/routes/orders.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import OrdersController from '../controllers/OrdersController';
+import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
+
+const ordersRoutes = Router();
+const ordersController = new OrdersController();
+
+ordersRoutes.use(isAuthenticated);
+
+//Utiliza o framework Celebrate como middleware para fazer a validação dos dados das rotas
+ordersRoutes.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  ordersController.show,
+);
+
+ordersRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      customer_id: Joi.string().uuid().required(),
+      products: Joi.required(),
+    },
+  }),
+  ordersController.create,
+);
+
+export default ordersRoutes;
