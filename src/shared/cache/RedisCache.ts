609e73a0d6540ca1f5115b6dacda58a0da23b794
Gustavo <bolatechproducoes@gmail.com>
2022-07-22 14:30:23 -0300

    Class Redis cache methods config

--- src/shared/cache/RedisCache.ts
+++ src/shared/cache/RedisCache.ts
@@ -8,11 +8,24 @@ export default class RedisCache {
     this.client = new Redis(cacheConfig.config.redis);
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public async save(key: string, value: any): Promise<void> {
     await this.client.set(key, JSON.stringify(value));
   }
 
-  //public async recover<T>(key: string): Promise<T | null> {}
+  public async recover<T>(key: string): Promise<T | null> {
+    const data = await this.client.get(key);
 
-  //public async invalidate(key: string): Promise<void> {}
+    if (!data) {
+      return null;
+    }
+
+    const parsedData = JSON.parse(data) as T;
+
+    return parsedData;
+  }
+
+  public async invalidate(key: string): Promise<void> {
+    await this.client.del(key);
+  }
 }
