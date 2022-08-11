76588a293670bab4e032db79c07e2e2b871ddc17
Gustavo <bolatechproducoes@gmail.com>
2022-08-11 13:41:09 -0300

    FakeCustomersRepository

--- src/modules/customers/services/CreateCustomerService.spec.ts
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -1,3 +1,8 @@
-it('should be 10', () => {
-  expect(5 + 5).toEqual(10);
+describe('createCustomer', () => {
+  it('should be able to create a new customer', () => {
+    expect(1).toBe(1);
+  });
+  it('should not be able to create two customers with the same email', () => {
+    expect(1).toBe(1);
+  });
 });
