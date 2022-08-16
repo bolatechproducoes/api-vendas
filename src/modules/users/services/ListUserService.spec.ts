47fb45c4df874181bf7bf08324102a255242e56e
G Alob <85072394+bolatechproducoes@users.noreply.github.com>
2022-08-16 12:18:10 -0300

    Add test: ListUserService.spec

---
+++ src/modules/users/services/ListUserService.spec.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata';
+import CreateUserService from './CreateUserService';
+import ListUserService from './ListUserService';
+import FakeUsersRepository from '../domain/repositories/fakes/FakeUserRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import User from '../infra/typeorm/entities/User';
+
+let fakeUsersRepository: FakeUsersRepository;
+let createUser: CreateUserService;
+let listUser: ListUserService;
+let fakeHashProvider: FakeHashProvider;
+
+describe('listUser', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+    createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+    listUser = new ListUserService(fakeUsersRepository);
+  });
+  it('should be able to list all users', async () => {
+    await expect(listUser.execute()).resolves.toBeInstanceOf(Array);
+  });
+});
